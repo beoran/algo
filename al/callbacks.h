af0f0ecd638b998a9af9aabf50d92b4eb7c2273b
Beoran <beoran@beoran.net>
2018-04-06 06:03:09 +0200

    Adding callbacks header.

--- al/callbacks.h
+++ al/callbacks.h
@@ -2,10 +2,14 @@
 #define ALGO_CALLBACKS_H
 
 
-/* wrapper around al_create_custom_bitmap */
+/* Callback for al_create_custom_bitmap */
 ALLEGRO_BITMAP * go_create_custom_bitmap(int w, int h, void * context);
 
+/* Callback for al_run_main_thread. */
 int go_run_main_callback(int argc, char ** argv);
+
+/* Callback for al_create_thread. */
+void * go_create_thread_callback(ALLEGRO_THREAD * thread, void * arg);
 
 
 #endif
